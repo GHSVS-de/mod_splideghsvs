17140a46396c13b08f3d4998c436c8f642e9ac3a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-07-09 17:30:58 +0200

    mod_splideghsvs.php. Remove garbage.

--- src/mod_splideghsvs.php
+++ src/mod_splideghsvs.php
@@ -1,31 +1,13 @@
 <?php
 defined('_JEXEC') or die;
 
-if (version_compare(JVERSION, '4', 'lt'))
-{
-	JLoader::registerNamespace(
-		'Joomla\\Module\\SplideGhsvs\\Site\\Helper',
-		__DIR__ . '/src/Helper',
-		false,
-		false,
-		'psr4'
-	);
-}
-
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\Module\SplideGhsvs\Site\Helper\SplideGhsvsHelper;
 
-$opts = null;
-$optsThumbs = null;
-$sliderType = $params->get('sliderType', '');
-
 $slides = SplideGhsvsHelper::getSlides($params);
 
-if (is_object($slides) && count(get_object_vars($slides)))
-{
-}
-else
+if ($slides === false)
 {
 	return;
 }
