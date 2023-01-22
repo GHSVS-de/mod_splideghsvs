f20b39b79b979194283a80fd73392c58e330e3da
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2023-01-22 02:36:53 +0100

    Add layout masemann.php
    using $slide->text and $slide->cssClass;

---
+++ src/tmpl/masemann.php
@@ -0,0 +1,66 @@
+<?php
+defined('_JEXEC') or die;
+
+$myId = 'image-slider-' . $module->id;
+
+$Config = [
+	'type' => (string) $config->get('type', 'fade'),
+	'speed' => (int) $config->get('speed', 4000),
+	'rewind' => (bool) $config->get('rewind', 1),
+	'width' => (string) $config->get('width', '100%'),
+	'arrows' => (bool) $config->get('arrows', 1),
+	'pagination' => (bool) $config->get('pagination', 0),
+	'autoplay' => (bool) $config->get('autoplay', 0),
+	'perPage' => (int) $config->get('perPage', 1),
+	'perMove' => (int) $config->get('perPage', 1),
+	'pauseOnHover' => (bool) $config->get('pauseOnHover', 1),
+	//'padding' => (string) $config->get('padding', 0),
+	//'keyboard' => (bool) $config->get('keyboard', 0),
+	'gap' => (string) $config->get('gap', '0px'),
+	'focus' => 'center',
+	/* 'breakpoints' => [
+		991.998 => [
+			'perPage' => 2
+		],
+		713 => [
+			'perPage' => 1,
+			'gap' => '0px'
+		],
+	], */
+];
+
+$js =
+"document.addEventListener( 'DOMContentLoaded', function () {"
+	. "new Splide('#" . $myId . "', " . json_encode($Config) . " ).mount();});";
+
+$wa->addInlineScript($js);
+?>
+<div class="d-block">
+<div id="<?php echo $myId; ?>" class="splide">
+	<div class="splide__track">
+		<ul class="splide__list">
+<?php
+foreach ($slides as $key => $slide)
+{
+	$wh = '';
+
+	if ($slide->width && $slide->height)
+	{
+		$wh = ' width=' . $slide->width . ' height=' . $slide->height;
+	} ?>
+			<li class="splide__slide slideOf-<?php echo $key; ?>">
+				<img src="<?php echo $slide->foto; ?>" alt=""<?php echo $wh; ?>>
+				<?php if ($slide->text )
+				{ ?>
+					<div class="<?php echo $slide->cssClass; ?>">
+						<?php echo $slide->text; ?>
+					</div>
+				<?php
+				} ?>
+			</li>
+<?php
+} ?>
+		</ul><!--/splide__list-->
+  </div><!--/splide__track-->
+</div><!--/#<?php echo $myId; ?>-->
+</div>
